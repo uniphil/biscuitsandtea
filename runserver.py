beb00c5ed479035e7cc62c656d73b5fbd83a79ae
jameh <jamie.alban@gmail.com>
2013-07-03 03:38:09 -0400

    Convert main.css to main.scss, so much nicer

--- runserver.py
+++ runserver.py
@@ -1,4 +1,12 @@
 from mysite import app
 
 if __name__ == '__main__':
-    app.run(debug=True, host="0.0.0.0")
+    app.debug=True
+    try:
+        from flask.ext.scss import Scss
+        Scss(app, static_dir="mysite/static/css",
+             asset_dir="mysite/static/scss")
+    except ImportError:
+        print("Not compiling scss, not installed.")
+
+    app.run(host="0.0.0.0")
